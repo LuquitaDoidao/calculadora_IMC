37506ec9725fb124955191f7532d0cd2022eddf5
LuquitaDoidao <lucasamarantelima7@gmail.com>
2025-02-03 10:41:49 -0300

    aaa

---
+++ teste.js
@@ -0,0 +1,1 @@
+// tinha um negocio aqui, mas como eu não sabia como apagar o arquivo, então eu apaguei apenas o conteudo
--- teste/teste.js
+++
@@ -1,1 +0,0 @@
-// tinha um negocio aqui, mas como eu não sabia como apagar o arquivo, então eu apaguei apenas o conteudo
